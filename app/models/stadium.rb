424941b6be16022fc172190ebca25e1f558527d9
frederic jeanne <fredericjeanne@gmail.com>
2023-01-19 22:28:41 +0100

    search form with dynamic map

--- app/models/stadium.rb
+++ app/models/stadium.rb
@@ -12,4 +12,16 @@ class Stadium < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_description_and_location,
+  against: [ :stadium_description, :location ],
+  using: { tsearch: { prefix: true } }
+
+  def is_available?(start_date, end_date)
+    bookings.each do |b|
+      return false if (b.starts_at..b.ends_at).overlaps?(start_date.to_date..end_date.to_date)
+    end
+    true
+  end
 end
